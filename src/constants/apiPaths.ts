32517a9a719dd30fa0779d8ae53934ad4299d636
DeHoKo <zenkovsk@gmail.com>
2021-10-08 00:59:42 +0300

    feat: add integration with s3

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://3kn94dbpil.execute-api.eu-west-1.amazonaws.com/dev/',
+  order: 'https://3kn94dbpil.execute-api.eu-west-1.amazonaws.com/dev/',
+  import: 'https://3kn94dbpil.execute-api.eu-west-1.amazonaws.com/dev/',
+  bff: 'https://3kn94dbpil.execute-api.eu-west-1.amazonaws.com/dev/',
+  cart: 'https://3kn94dbpil.execute-api.eu-west-1.amazonaws.com/dev/',
 };
 
 export default API_PATHS;
