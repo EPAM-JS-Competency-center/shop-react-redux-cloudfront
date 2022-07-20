ab1fb47ecfd0fb50038c1d7480250ed95e8f9a1b
ShinkarenkoMaxim <shinkarenko.max97@gmail.com>
2022-07-20 23:41:01 +0500

    Fix import api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import:
-    'https://4y332cvtxd.execute-api.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://4y332cvtxd.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://iv5t4k1mkd.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
