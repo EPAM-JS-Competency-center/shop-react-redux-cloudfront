1c00860428cef18f644850b3e44c73a721f75cac
Sergio Domínguez <sergio.dominguez@sumamexico.com>
2022-09-24 19:27:00 -0500

    Task -3 completed

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -2,9 +2,11 @@ import * as Yup from "yup";
 
 export const ProductSchema = Yup.object({
   id: Yup.string(),
-  title: Yup.string().required().default(""),
+  name: Yup.string().required().default(""),
   description: Yup.string().default(""),
   price: Yup.number().positive().required().defined().default(0),
+  imageUrl: Yup.string().notRequired().url().default(""),
+  quantity: Yup.number().positive().notRequired().defined().default(0),
 });
 
 export const AvailableProductSchema = ProductSchema.shape({
