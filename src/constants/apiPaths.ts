11954acdf3857d8d6e2765d5a9253cb9edda6a86
VVS <teenwolf23299@gmail.com>
2022-10-09 13:11:48 +0300

    task 5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://wtmasbu4ze.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://ml8i1o232a.execute-api.eu-west-1.amazonaws.com/dev/products',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
 };
