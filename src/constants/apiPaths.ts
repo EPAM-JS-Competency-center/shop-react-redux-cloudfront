df47592cecf8903301a2ceec981c7aca302192fb
tarik_ceylan <tarikceylan@outlook.com.tr>
2023-03-04 22:18:41 +0300

    Complete Task 4

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product:
-    'https://880r7dykid.execute-api.eu-central-1.amazonaws.com/dev/products',
+    'https://ehfjwwye8l.execute-api.eu-central-1.amazonaws.com/dev/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://880r7dykid.execute-api.eu-central-1.amazonaws.com/dev/products',
+  bff: 'https://ehfjwwye8l.execute-api.eu-central-1.amazonaws.com/dev/products',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
