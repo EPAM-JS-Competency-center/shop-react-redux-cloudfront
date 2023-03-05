7a770b029ab93e482b2953cea572862c546e7c55
Maryna <sollferino@gmail.com>
2023-03-06 01:45:14 +0400

    task: added serverless config
    Service is done, FE looks ok
    Cloudfront link: https://d2k34eug3qntvy.cloudfront.net/

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store!
+            My Store for cutest things!!
           </Link>
         </Typography>
 
