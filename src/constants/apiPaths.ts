b6d16ddb486f9a7d5e3c195e68d637cc5d4146ae
tarik_ceylan <tarikceylan@outlook.com.tr>
2023-03-11 19:37:25 +0300

    Complete Task 5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,8 @@ const API_PATHS = {
   product:
     'https://ehfjwwye8l.execute-api.eu-central-1.amazonaws.com/dev/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import:
+    'https://3nri5xjq8c.execute-api.eu-central-1.amazonaws.com/dev/import',
   bff: 'https://ehfjwwye8l.execute-api.eu-central-1.amazonaws.com/dev/products',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
 };
