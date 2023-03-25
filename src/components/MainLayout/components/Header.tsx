df39381fa52a30c6d5c30c4a314c0ce9660f53c1
Dzmitry <Dzmitry_Chervanenka@epam.com>
2023-03-25 16:42:04 +0300

    changed header

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store!
+            Dzmitry's store!!!
           </Link>
         </Typography>
 
