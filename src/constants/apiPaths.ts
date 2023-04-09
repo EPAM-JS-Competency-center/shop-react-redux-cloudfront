4aae63fbe35ded4577a1a6f31234804ad0d5c0b6
Alexander Borodavka <realgvard@gmail.com>
2023-04-09 18:24:04 +0400

    Merge 'task-5' into master

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://mtfepoga1d.execute-api.eu-west-1.amazonaws.com",
+  order: "https://mtfepoga1d.execute-api.eu-west-1.amazonaws.com",
+  import: "https://ey83z0bt11.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://mtfepoga1d.execute-api.eu-west-1.amazonaws.com",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://mtfepoga1d.execute-api.eu-west-1.amazonaws.com",
 };
 
 export default API_PATHS;
