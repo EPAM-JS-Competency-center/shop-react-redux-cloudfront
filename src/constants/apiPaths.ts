9c0fd5cdd31eef58ccb5d7bd29a3fc79540bf646
Daniil Tarantsei <daniil_tarantsei@epam.com>
2023-04-19 01:16:31 +0300

    feat: add integration with imports service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
-  product: "https://kshs988yui.execute-api.us-east-1.amazonaws.com",
+  product: "https://88xlgxv3hh.execute-api.us-east-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://n3thyx7fnh.execute-api.us-east-1.amazonaws.com",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
