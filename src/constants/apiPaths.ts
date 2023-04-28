46419a522a0eea7e9ee65fb40a7ee505782f1f4e
Dzmitry Chervanenka <Dzmitry_Chervanenka@epam.com>
2023-04-28 10:52:39 +0300

    implement task-5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://uh807vv08h.execute-api.eu-north-1.amazonaws.com/dev",
   order: "https://uh807vv08h.execute-api.eu-north-1.amazonaws.com/dev",
-  import: "https://uh807vv08h.execute-api.eu-north-1.amazonaws.com/dev",
+  import: "https://7ao7ydvxqi.execute-api.eu-north-1.amazonaws.com/dev",
   bff: "https://uh807vv08h.execute-api.eu-north-1.amazonaws.com/dev",
   cart: "https://uh807vv08h.execute-api.eu-north-1.amazonaws.com/dev",
 };
