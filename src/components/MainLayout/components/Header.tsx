fd3309de61185f982da3f7359943ccc03c82e38b
Volodymyr Harhay <harhayv@buildone.co>
2023-09-29 18:27:16 +0300

    added serverless-single-page-app-plugin

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store!
+            My Greatest Store!
           </Link>
         </Typography>
 
