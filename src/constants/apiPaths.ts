61403ea144d584f5098469fc2c3d9e73106408ff
mukeshsarma054@gmail.com <mukeshsharma054@gmail.com>
2023-11-06 19:04:28 +0530

    Initial commit - Task 7

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://uyqbchsb3k.execute-api.us-east-1.amazonaws.com/dev/import",
-  bff: "https://4wmkmaho8l.execute-api.us-east-1.amazonaws.com/products",
+  import: "https://wugzf9p728.execute-api.us-east-1.amazonaws.com/dev/import",
+  bff: "https://s6acflgetj.execute-api.us-east-1.amazonaws.com/products",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
