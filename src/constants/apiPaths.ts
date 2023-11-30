7c3114645ae4325439a5b1806c8bcb6e04b9519e
fepu08 <ferencbesenyei@protonmail.com>
2023-11-30 21:00:19 +0100

    aadded cart path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://0trvutezp4.execute-api.eu-central-1.amazonaws.com",
-  order: "https://0trvutezp4.execute-api.eu-central-1.amazonaws.com",
+  product: "https://9p7hw0bf7e.execute-api.eu-central-1.amazonaws.com",
+  order: "https://9p7hw0bf7e.execute-api.eu-central-1.amazonaws.com",
   import: "https://cfpa3haqo0.execute-api.eu-central-1.amazonaws.com/dev",
-  bff: "https://0trvutezp4.execute-api.eu-central-1.amazonaws.com",
-  cart: "https://0trvutezp4.execute-api.eu-central-1.amazonaws.com",
+  bff: "https://9p7hw0bf7e.execute-api.eu-central-1.amazonaws.com",
+  cart: "https://zu4hmxuhhk.execute-api.eu-central-1.amazonaws.com/dev/",
 };
 
 export default API_PATHS;
