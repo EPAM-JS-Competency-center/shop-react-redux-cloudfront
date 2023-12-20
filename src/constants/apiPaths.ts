66a612393be1d0df801fcaaa9436f3806e2ea078
Jose Miguel Carvajal Jimenez <josemiguel_carvajal@epam.com>
2023-12-20 11:24:57 -0500

    task-6

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://bsoxq54hhi.execute-api.us-east-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://bsoxq54hhi.execute-api.us-east-1.amazonaws.com",
   bff: "https://bsoxq54hhi.execute-api.us-east-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
